b1aa751c37f4dec6dd641e4369d8c011f7688465
miguelnsimoes <miguelnazariosimoes@gmail.com>
2025-12-13 17:51:49 -0300

    codando models do banco sqlalchemy, e migracao banco alembic

---
+++ models.py
@@ -0,0 +1,69 @@
+from sqlalchemy import create_engine, Column, String, Integer, Boolean, Float, ForeignKey
+from sqlalchemy.orm import declarative_base
+from sqlalchemy_utils.types import ChoiceType
+
+
+#conexao do banco
+db = create_engine("sqlite:///banco.db")
+
+#base do banco
+Base = declarative_base()
+
+#cria as tabeças do banco
+class Usuario(Base):
+    __tablename__ = "usuarios"
+
+    id = Column("id", Integer, primary_key=True, autoincrement=True)
+    nome = Column("nome", String)
+    email = Column("email", String, nullable=False)
+    senha = Column("senha", String)
+    ativo = Column("ativo", Boolean)
+    admin = Column("admin", Boolean, default=False)
+
+    def __init__(self, nome,  email, senha, ativo, admin=False):
+        self.nome = nome
+        self.email = email
+        self.senha = senha
+        self.ativo = ativo
+        self.admin = admin
+
+
+class Pedido(Base):
+    __tablename__ = "pedidos"
+
+    #tupla de tuplas do ChoiceType pra definir os status do pedido 
+    #STATUS_PEDIDOS = (
+    #    ("PENDENTE", "PENDENTE"),
+    #    ("CANCELADO", "CANCELADO"),
+    #    ("FINALIZADO", "FINALIZADO")
+    #)
+
+    id = Column("id", Integer, primary_key=True, autoincrement=True)
+    status = Column("status", String)
+    usuario = Column("usuario", ForeignKey("usuarios.id"))
+    preco = Column("preco", Float)
+    
+    def __init__(self, usuario, status="PENDENTE", preco=0):
+        self.usuario = usuario
+        self.preco = preco
+        self.status = status
+
+
+class ItemPedido(Base):
+    __tablename__ = "pedido_itens"
+
+    id = Column("id", Integer, primary_key=True, autoincrement=True)
+    quantidade = Column("quantidade", Integer)
+    sabor = Column("Sabor", String)
+    tamanho = Column("tamanho", String)
+    preco_unitario = Column("preco_unitario", Float)
+    pedido = Column("pedido", ForeignKey("pedidos.id"))
+
+    def __init__(self, quantidade, sabor, tamanho, preco_unitario, pedido):
+        self.quantidade = quantidade
+        self.sabor = sabor
+        self.tamanho = tamanho
+        self.preco_unitario = preco_unitario
+        self.pedido = pedido
+#comandos para processo de migracao de banco de dados (alembic revision --autogenerate -m "mensagem")
+#executa a criacao dos metadados do banco
