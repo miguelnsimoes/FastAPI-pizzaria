455769805fca1a4b15db63c0999c277fb0a008ba
miguelnsimoes <miguelnazariosimoes@gmail.com>
2025-12-14 17:53:21 -0300

    gerenciamento de sessoes com Depends, rota de criar conta, logica sessionmaker

--- dependencies.py
+++ dependencies.py
@@ -3,6 +3,10 @@ from sqlalchemy.orm import sessionmaker
 
 #criar sessao no bd e dar como parametro nas rotas
 def pegar_sessao():  
-    Session = sessionmaker(bind=db)
-    session = Session()
-    return session
+    try:
+        Session = sessionmaker(bind=db)
+        session = Session()
+        yield session
+        session.close
+    finally:
+        session.close()
