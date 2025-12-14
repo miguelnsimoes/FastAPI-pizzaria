67e3a90910c732a0b48b79611709cfac3fbecbc8
miguelnsimoes <miguelnazariosimoes@gmail.com>
2025-12-14 19:48:28 -0300

    criacao rota_pedido para fazer pedido e vincular o id do usuario

--- rotas_pedidos.py
+++ rotas_pedidos.py
@@ -1,4 +1,8 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+from dependencies import pegar_sessao
+from schemas import PedidoSchema
+from models import Pedido
 
 rota_pedido = APIRouter(prefix="/pedidos", tags=["pedido"])
 
@@ -9,4 +13,14 @@ async def pedidos():
     """
     return {
         "mensagem": "voce acessou a rota de pedidos"
+    }
+
+
+@rota_pedido.post("/pedido")
+async def criar_pedido(pedido_schema: PedidoSchema, session: Session = Depends(pegar_sessao)):
+    novo_pedido = Pedido(usuario=pedido_schema.id_usuario)
+    session.add(novo_pedido)
+    session.commit()
+    return {
+        "mensagem": f"pedido criado com sucesso. ID do pedido {novo_pedido.id}"
     }
